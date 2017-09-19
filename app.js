780110f7b1f63f41289528037373597ed6019c75
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 17:59:22 -0600

    commented appendfile

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var user = os.userInfo();
 
 var res = notes.addNote();
 
-fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.` , (err) => {
-  if (err) throw err;
-  console.log('The "data to append" was appended to file!');
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.` , (err) => {
+//   if (err) throw err;
+//   console.log('The "data to append" was appended to file!');
+// });
