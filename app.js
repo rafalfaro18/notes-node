89f8e92db3be2d992cfb972f3729814a9ed5b04f
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 16:56:49 -0600

    added callback to appendfile

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log('starting app.');
 const fs = require('fs');
-fs.appendFile('greetings.txt','Hello World!');
+fs.appendFile('greetings.txt','Hello World!', (err) => {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+});
