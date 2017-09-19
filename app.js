ee69be9da4a4a557dc00545d36dedf6a49e73819
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 17:50:14 -0600

    Use of the age property exported in notes.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username} !` , (err) => {
+fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.` , (err) => {
   if (err) throw err;
   console.log('The "data to append" was appended to file!');
 });
