9f4b318de0347643c49a6a3ab2ac99249196bad1
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 17:58:46 -0600

    declared variable res

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const notes = require('./notes.js');
 
 var user = os.userInfo();
 
+var res = notes.addNote();
+
 fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.` , (err) => {
   if (err) throw err;
   console.log('The "data to append" was appended to file!');
