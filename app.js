1efe91b3bef613a25385c5087f0553715795ca36
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 17:05:31 -0600

    require os

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log('starting app.');
+
 const fs = require('fs');
+const os = require('os');
+
 fs.appendFile('greetings.txt','Hello World!', (err) => {
   if (err) throw err;
   console.log('The "data to append" was appended to file!');
