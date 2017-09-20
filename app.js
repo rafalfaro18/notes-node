b7edb667f3ca4ecef8f570a8a377126158c7a07e
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 21:23:33 -0600

    compare command

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const notes = require('./notes.js');
 
 var command = process.argv[2];
 console.log(command);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else {
+  console.log('Command not recognized');
+}
