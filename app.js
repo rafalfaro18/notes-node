cae07cf49117863b12dc0e1532feddd9afe027f5
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-09-19 20:57:54 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-var user = os.userInfo();
-
-var filreredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4]);
-console.log(filreredArray);
-
-// console.log(_.isString(true));
-// console.log(_.isString('Rafael'));
-
-//console.log('Result: ',notes.add(9,-2));
-
-// fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.` , (err) => {
-//   if (err) throw err;
-//   console.log('The "data to append" was appended to file!');
-// });
+const notes = require('./notes.js');
